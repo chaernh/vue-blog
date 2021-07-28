16b605e83105322bc9334df8d2fc68192cdb3ac2
CHAER <chaerfansyah.hidayah@gmail.com>
2021-07-28 10:26:32 +0700

    make a ui for index

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -8,10 +8,50 @@
                     </router-link>
                 </div>
                 <div class="right">
-                    Login
+                    <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">Login</button>
                 </div>
             </div>
         </nav>
+    </div>
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <div class="login-box row">
+                        <div class="col-md-12">
+                            <div class="header text-center">
+                                <img src="../assets/logo.png" alt="logo">
+                                <figure class="text-center">
+                                    <blockquote class="blockquote">
+                                        <p>A well-known quote, contained in a blockquote element.</p>
+                                    </blockquote>
+                                    <figcaption class="blockquote-footer">
+                                        Someone famous in <cite title="Source Title">Source Title</cite>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                            <hr>
+                            <div class="mb-3">
+                                <label for="username" class="form-label">Username</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password</label>
+                                <input type="password" class="form-control">
+                            </div>
+                            <div class="d-grid mb-3">
+                                <button class="btn btn-success">Login</button>
+                            </div>
+                            <hr>
+                            <div class="text-center">
+                                <p>Don't have an account? Sign up <a href="#">here</a>.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
